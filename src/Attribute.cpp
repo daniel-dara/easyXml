d3eb0337edf2f30405b5b100bf1ddaea13dd83c7
Daniel Dara <ddara@g.clemson.edu>
2017-10-23 20:49:59 -0400

    Added member to initialization list in Attribute constructor.

--- src/Attribute.cpp
+++ src/Attribute.cpp
@@ -5,7 +5,8 @@ namespace EASYXML_NAMESPACE
 {
 	Attribute::Attribute(const String& name, const String& value) :
 		name(name),
-		value(value)
+		value(value),
+		next_sibling_(NULL)
 	{ }
 
 	Attribute* Attribute::getNextSibling() {
